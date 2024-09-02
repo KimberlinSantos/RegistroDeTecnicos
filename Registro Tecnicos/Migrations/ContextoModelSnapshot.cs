1d5c26ac66a5ad95fda3e04138bc4491e4d8a589
KimberlinSantos <144037018+KimberlinSantos@users.noreply.github.com>
2024-09-02 15:33:56 -0400

    Add files via upload

---
+++ Registro Tecnicos/Migrations/ContextoModelSnapshot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using RegistroTecnicos.DAL;
+
+#nullable disable
+
+namespace RegistroTecnicos.Migrations
+{
+    [DbContext(typeof(Contexto))]
+    partial class ContextoModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+#pragma warning disable 612, 618
+            modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
+
+
+            
+           
+
+            modelBuilder.Entity("RegistroTecnicos.Models.Incentivos", b =>
+            {
+                
+               
+               
+
+                b.Navigation("Tecnicos");
+            });
+
+
+            
+
+            modelBuilder.Entity("RegistroTecnicos.Models.TiposTecnicos", b =>
+            {
+                b.Navigation("Tecnicos");
+            });
+#pragma warning restore 612, 618
+        }
+    }
+}
---
+++ Registro Tecnicos/Program.cs
@@ -0,0 +1,46 @@
+using RegistroTecnicos.Components;
+using RegistroTecnicos.DAL;
+using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.Services;
+using Registro_Tecnicos.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddSingleton<Radzen.NotificationService>();
+builder.Services.AddBlazorBootstrap();
+
+
+//Obtener el constructor
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+
+//Agregamos el contexto
+builder.Services.AddDbContext<Contexto>(Options => Options.UseSqlite(ConStr));
+
+//Inyectar el service
+
+builder.Services.AddScoped<TecnicosServices>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
+
